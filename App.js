94d7e9f48ca082805901bf5dc91864c64e9296f3
nawazokz <muhammadnawaz6261@gmail.com>
2024-07-08 17:17:41 +0500

    add app.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './src/store/store';
+import AppNavigator from './src/navigation/AppNavigator';
+const App = () => (
+  <Provider store={store}>
+    <AppNavigator />
+  </Provider>
+);
+
+export default App;
